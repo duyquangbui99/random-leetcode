2d720e4b04cf6ae34ddaed1620d297fb3d9731e5
Quang Bui <77463586+duyquangbui99@users.noreply.github.com>
2024-12-07 12:32:59 -0600

    Update 1957.py

--- 1957.py
+++ 1957.py
@@ -15,4 +15,21 @@ def makeFancyString(s):   # beat only 46.65% in time complexity
 
     return newString
 
+#Alternative method: using array instead of string concatenation making time complexity from O(n^2) to O(n)
+def makeFancyString(s):  # beat 86%, much better
+    newString = [s[0]]
+    count = 1
+    for i in range(1, len(s)):
+        c = s[i]
+        if c == newString[-1] and count < 2:
+            newString.append(c)
+            count += 1
+        elif c != newString[-1]:
+            newString.append(c)
+            count = 1
+        else:
+            continue
+
+    return "".join(newString)
+
 
