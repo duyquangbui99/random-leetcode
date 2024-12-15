9419dd1fbb43d94e571d78c5f404b3db815cdf90
Quang Bui <77463586+duyquangbui99@users.noreply.github.com>
2024-12-15 10:10:59 -0600

    Update 2490.py

--- 2490.py
+++ 2490.py
@@ -1,2 +1,11 @@
-def isCircularSentence(sentence):
-  return
+def isCircularSentence(sentence): # beat 100% in time complexity 
+  if sentence[0] != sentence[-1]:
+      return False
+  arr = sentence.split()
+  char = arr[0][-1]
+  for i in range(1, len(arr)):
+      if char == arr[i][0]:
+          char = arr[i][-1]
+      else:
+          return False
+  return True
