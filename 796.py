1da8d30376692bbcb03423f04176fe1a6ea40c2c
Quang Bui <77463586+duyquangbui99@users.noreply.github.com>
2024-12-18 11:59:37 -0600

    Update 796.py

--- 796.py
+++ 796.py
@@ -1,4 +1,4 @@
-def rotateString(s, goal):  #beat 100% in time complexity
+def rotateString(s, goal):  #beat 100% in time complexity, however it still slow time: O(n^2)
     if len(s) != len(goal):
         return False
 
@@ -8,3 +8,13 @@ def rotateString(s, goal):  #beat 100% in time complexity
             return True
 
     return False
+
+#Optimal solution:
+def rotateString(self, s, goal):
+    return len(s) == len(goal) and goal in (s + s)
+
+#Time complexity: O(n)
+#Concatenating s + s takes O(n), where n is the length of s.
+#Checking if goal is a substring takes O(n).
+#Thus, the overall time complexity is O(n).
+
